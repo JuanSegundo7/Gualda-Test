ceedb0cc28c22e91f7fb027542f1c4612b05f049
Juan Segundo <juansepincha@gmail.com>
2023-04-13 18:40:19 -0300

    adding some styles and characters folder

--- src/components/films/index.tsx
+++ src/components/films/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Films } from "../../../models/types";
+import styles from "./filmCard.module.css";
 
-function FilmCard({ title, episode_id }: Films) {
+function FilmCard({ title, episode_id, director }: Films) {
   return (
-    <div>
+    <div id={styles.FilmCard}>
       <h1>{title}</h1>
-      <p>Episode number: {episode_id}</p>
+      <p>Episode number {episode_id}</p>
+      <p>Director: {director}</p>
       <p>Characters</p>
     </div>
   );
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch } from "../../redux/hook";
 import { useSelector } from "react-redux";
 import { getFilms } from "../../redux/actions";
 import { Films, States } from "../../../models/types";
+import styles from "./home.module.css";
 import FilmCard from "../films";
 
 const Home = () => {
@@ -14,17 +15,18 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <section>
-      <h1>Home</h1>
-      {films.length > 0 &&
-        films.map((films: Films) => (
-          <FilmCard
-            title={films.title}
-            episode_id={films.episode_id}
-            director={films.director}
-            characters={films.characters}
-          />
-        ))}
+    <section id={styles.Home}>
+      <div className={styles.filmsFlex}>
+        {films.length > 0 &&
+          films.map((films: Films) => (
+            <FilmCard
+              title={films.title}
+              episode_id={films.episode_id}
+              director={films.director}
+              characters={films.characters}
+            />
+          ))}
+      </div>
     </section>
   );
 };
